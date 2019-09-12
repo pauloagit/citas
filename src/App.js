691e7cb6957a793d68f4b3a58c94172e75019601
Armando Paulo <armando.paulo@walmart.com>
2019-09-12 08:07:41 -0300

    First update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // useState retorna 2 funciones
+    const [citas, guardarCitas] = useState([]);
+
+    console.log(citas);
+
+    return(
+        <h1>Hola!!</h1>
+    )
 }
 
 export default App;
